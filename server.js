32193ea2f5ab8c92e5d23c572245ea2778fc7b84
Summer <svilleneuve1994@gmail.com>
2021-11-05 15:58:59 -0400

    updating ports

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-// app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
